badc3db9f7415ca5d6374a84c9c24b150f1f79fb
MridulTailor <mtailor729@gmail.com>
2022-11-16 15:35:00 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/components/home_page.dart';
-import 'package:flutter_application_1/components/testing_page.dart';
 import 'firebase_options.dart';
 import 'components/login_page.dart';
 
